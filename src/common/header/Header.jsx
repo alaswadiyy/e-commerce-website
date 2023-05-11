056a9d69d4b3a2d36548a95cc783fb7bba392ecd
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-05-11 21:09:52 +0100

    second

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Head from './Head'
+import Search from './Search'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
     <>
-        <h1>Header</h1>
+        <Head />
+        <Search />
+        <Navbar />
     </>
   )
 }
